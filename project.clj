ca8a20b73fa5d0763847ed496d64f65d435eee15
jmgpena <jorge@jmgpena.net>
2015-03-03 08:00:05 +0000

    Add cegdown and garden dependencies
    cegdown - markdown parser
    garden - css creation (like hiccup)

--- project.clj
+++ project.clj
@@ -6,7 +6,10 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [stasis "2.2.2"]
                  [ring "1.3.2"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [me.raynes/cegdown "0.1.1"]
+                 [garden "1.2.5"]
+                 [org.clojure/tools.nrepl "0.2.7"]]
   :plugins [[lein-ring "0.9.2"]]
   :ring {:handler jmgpena-net.core/app}
   :aliases {"build-site" ["run" "-m" "jmgpena-net.core/export"]})
